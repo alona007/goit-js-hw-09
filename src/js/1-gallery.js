b841cec6f4736fe151e40afdfb05b433a79332de
alona007 <alona.olyksashenko@gmail.com>
2024-04-29 21:37:51 +0200

    3

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,90 +1,87 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 const images = [
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    alt: 'Hokkaido Flower',
+    description: 'Hokkaido Flower',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-   alt: 'Container Haulage Freight',
+    description: 'Container Haulage Freight',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    alt: 'Aerial Beach View',
+    description: 'Aerial Beach View',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    alt: 'Flower Blooms',
+    description: 'Flower Blooms',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    alt: 'Alpine Mountains',
+    description: 'Alpine Mountains',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    alt: 'Mountain Lake Sailing',
+    description: 'Mountain Lake Sailing',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    alt: 'Alpine Spring Meadows',
+    description: 'Alpine Spring Meadows',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    alt: 'Nature Landscape',
+    description: 'Nature Landscape',
   },
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    alt: 'Lighthouse Coast Sea',
+    description: 'Lighthouse Coast Sea',
   },
 ];
 
-const galleryItem = document.querySelector('.gallery'); 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryEl = document.querySelector('ul.gallery');
 
-const animations = images.map(image => { 
-    return `<li class = "gallery-item" >
-    <a class="gallery-link" href= ${image.original}>
-	<img
-			class = "gallery-image"  
-			src = "${image.preview}"  
-			alt = "${image.alt}"
-    />
+const galleryMarkup = images
+  .map(
+    image => `<li class="gallery-item">
+	<a class="gallery-link" href="${image.original}">
+		<img 
+			class="gallery-image" 
+			src="${image.preview}" 
+			alt="${image.description}" 
+			/>
 	</a>
-</li>` 
-}).join(''); 
-galleryItem.innerHTML = animations; 
+</li>\n`
+  )
+  .join('');
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionPosition: 'bottom',
-  captionDelay: 250,
-    captionsData: 'alt',
-    caption: true,
-});
+const galleryHtml = galleryEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+const lightBox = new SimpleLightbox('.gallery-item a', { captions: true, captionsData: "alt", captionDelay: 250 });
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,31 +1,46 @@
-const inputText = document.querySelector("input");
-inputText.addEventListener('click', (event) => {
-    event.target.setAttribute('placeholder', `Type area`);
-    event.target.style.border = '1px solid #808080';
-    event.target.classList.add('label-form');
-});
-inputText.addEventListener('blur', (event) => { 
-    event.target.setAttribute('placeholder', ``);
-});
-
-const FEEDBACK_FORM = 'feedback-form-state';
-const form = document.querySelector(".feedback-form");
-
-const formData = JSON.parse(localStorage.getItem(FEEDBACK_FORM)) || {};
-form.elements.email.value = formData.email || '';
-form.elements.message.value = formData.message || '';
-
-form.addEventListener('input', (event) => {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formData));
-});
-
-form.addEventListener('submit', (event) => {
-    if (event.target.elements.email.value === "" || event.target.elements.message.value === "") {
-        return alert(`Fill out all form fields!`);
+const addFont = document.head.insertAdjacentHTML(
+    'beforeend',
+    '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>'
+  );
+  
+  const formEl = document.querySelector('.feedback-form');
+  
+  const inputEvent = formEl.addEventListener('input', event => {
+    const formData = {
+      email: formEl.elements.email.value.trim(),
+      message: formEl.elements.message.value.trim(),
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  });
+  
+  const checkStorage = window.addEventListener('load', () => {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+      const { email, message } = JSON.parse(storedData);
+      formEl.elements.email.value = email;
+      formEl.elements.message.value = message;
     }
-    event.preventDefault();
-    console.log(`${event.target.elements.email.name}: ${event.target.elements.email.value.trim()}\n${event.target.elements.message.name} ${event.target.elements.message.value.trim()}`);
-    form.reset();
-    localStorage.removeItem(FEEDBACK_FORM);
-});
+  });
+  
+  const submitForm = formEl.addEventListener('submit', event => {
+    const emailValue = formEl.elements.email.value.trim();
+    const messageValue = formEl.elements.message.value.trim();
+    if (
+      emailValue.includes('@') &&
+      emailValue.includes('.') &&
+      emailValue &&
+      messageValue
+    ) {
+      event.preventDefault();
+      const formData = {
+        email: formEl.elements.email.value.trim(),
+        message: formEl.elements.message.value.trim(),
+      };
+      localStorage.removeItem('feedback-form-state');
+      formEl.reset();
+      console.log(formData);
+    } else {
+      event.preventDefault();
+      alert('Type valid email and message');
+    }
+  });
